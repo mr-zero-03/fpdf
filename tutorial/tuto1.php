d7e79cb4288b85548fcdbcc3d1f4378c5426c5f4
mr-zero-03 <danielsantiagoperez03@gmail.com>
2021-08-12 16:37:28 -0500

    I made some tests in the tutorial1

--- tutorial/tuto1.php
+++ tutorial/tuto1.php
@@ -1,9 +1,18 @@
 <?php
-require('fpdf.php');
+require('../fpdf.php');
 
-$pdf = new FPDF();
+$pdf = new FPDF('L', 'mm','Letter');
 $pdf->AddPage();
+$pdf->SetFont('Arial','I',25);
+$pdf->Cell(80, 10, null, 1);
+$pdf->Cell(100, 10,'ABCDEFGHIJKLMNOPQRSTUVXYZ!', 1, null, 'L', false, 'www.google.com'	);
+$pdf->Cell(80, 10, null, 1);
+$pdf->Ln(30);
+$pdf->SetFont('Arial','BIU',16);
+$pdf->Cell(42, 10,'OTRO STRING', 1);
+
+$pdf->AddPage('P', 'A4', 270);
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(40,10,'PAGE 2!');
 $pdf->Output();
 ?>
