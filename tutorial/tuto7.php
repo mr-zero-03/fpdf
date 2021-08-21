fa427ba3eb7f2ce22f8730310a88d21d8bb7cc93
mr-zero-03 <danielsantiagoperez03@gmail.com>
2021-08-21 16:35:31 -0500

    Test in the tutorial 7 (Changed the font from Calligrapher to Roboto)

--- tutorial/tuto7.php
+++ tutorial/tuto7.php
@@ -3,9 +3,12 @@ define('FPDF_FONTPATH','.');
 require('../fpdf.php');
 
 $pdf = new FPDF();
-$pdf->AddFont('Calligrapher','','calligra.php');
+$pdf->AddFont('Roboto','','Roboto-Black.php');
 $pdf->AddPage();
-$pdf->SetFont('Calligrapher','',35);
+$pdf->SetFont('Roboto','',35);
 $pdf->Cell(0,10,'Enjoy new fonts with FPDF!');
+$pdf->SetFont('Roboto','',12);
+$pdf->Ln(20);
+$pdf->Cell(0,10,'Now I am using Roboto');
 $pdf->Output();
 ?>
